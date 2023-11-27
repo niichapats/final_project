69cb872c821095e49d656eb19210d883ccc44269
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2023-11-28 00:11:20 +0700

    task2,3

--- database.py
+++ database.py
@@ -5,16 +5,76 @@ import csv, os
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
+
+class CSV:
+    def __init__(self):
+        self.data = []
+
+    def read(self, file_name):
+        with open(os.path.join(__location__, file_name)) as f:
+            rows = csv.DictReader(f)
+            for r in rows:
+                self.data.append(dict(r))
+        return self.data
+
+# def read(file_name):
+#     list_result = []
+#     with open(os.path.join(__location__, file_name)) as f:
+#         rows = csv.DictReader(f)
+#         for r in rows:
+#             list_result.append(dict(r))
+#     return list_result
+
 
 # add in code for a Database class
 
+class DB:
+    def __init__(self):
+        self.database = []
+
+    def insert(self, table):
+        self.database.append(table)
+
+    def search(self, table_name):
+        for table in self.database:
+            if table.table_name == table_name:
+                return table
+        return None
+
 # add in code for a Table class
+
+
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+        self.list = []
+
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+
+    def insert(self, item):
+        self.table.append(item)
+
+    def update(self, key, value, dict_name):
+        self.table.index(dict_name)[key] = value
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
 
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
 
--- project_manage.py
+++ project_manage.py
@@ -1,34 +1,65 @@
 # import database module
 
-# define a funcion called initializing
+import random
 
+# import database module
+import csv, os
+from database import CSV, DB, Table
+
+database = DB()
 def initializing():
-    pass
 
 # here are things to do in this function:
 
     # create an object to read all csv files that will serve as a persistent state for this program
+    read_csv = CSV()
+    persons = read_csv.read('persons.csv')
+    login = read_csv.read('login.csv')
 
     # create all the corresponding tables for those csv files
 
     # see the guide how many tables are needed
+    persons_table = Table('persons', persons)
+    login_table = Table('login', login)
 
     # add all these tables to the database
+    database.insert(persons_table)
+    database.insert(login_table)
+    print(persons_table)
+    print(login_table)
+
+
 
 
 # define a funcion called login
 
+
 def login():
-    pass
+    username = input('Enter username : ')
+    password = input('Enter password: ')
+    login_table = database.search('login')
+    result = login_table.filter(lambda data: data['username'] == username and data['password'] == password)
+    if len(result.table) != 0:
+        return [result.table[0]['ID'], result.table[0]['role']]
+    return None
 
 # here are things to do in this function:
    # add code that performs a login task
         # ask a user for a username and password
         # returns [ID, role] if valid, otherwise returning None
 
+
 # define a function called exit
+
 def exit():
-    pass
+    writer = CSV()
+    export_path = 'export/'
+    writer.write(export_path + 'persons.csv', database.search('persons').select('*'))
+    writer.write(export_path + 'login.csv', database.search('login').select('*'))
+    writer.write(export_path + 'project.csv', database.search('project').select('*'))
+    writer.write(export_path + 'advisor_pending_request.csv', database.search('advisor_pending_request').select('*'))
+    writer.write(export_path + 'member_pending_request.csv', database.search('member_pending_request').select('*'))
+
 
 # here are things to do in this function:
    # write out all the tables that have been modified to the corresponding csv files
@@ -41,8 +72,46 @@ def exit():
 
 initializing()
 val = login()
+print(val)
 
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
+
+
+class Student:
+    def __init__(self):
+        self.role = 'student'
+        self.num_request = 1
+        self.name_request = ''
+
+    def first_menu(self):
+        print(f'1.see request : you have {self.num_request} request(s)')
+        print(f'2.create project')
+        ans = int(input('>> '))
+        return ans
+
+    def see_request(self):
+        print(f'you have {self.num_request} from {self.name_request}')
+        print('1.accept')
+        print('2.dany')
+
+    def deny_request(self):
+        pass
+
+    def accept_request(self):
+        pass
+
+
+    def to_lead(self):
+        id_confirm = input('Enter your id to confirm project creation : ')
+        if val[0] == id_confirm:
+            self.role = 'lead'
+            login_table = Table('login', database.search('login_table'))
+            login_table.update('role', 'lead', id_confirm)
+        else:
+            print('Invalid id, can not crate a project.')
+
+    # def create_project(self, lead):
+
 
 # if val[1] = 'admin':
     # see and do admin related activities
@@ -56,6 +125,26 @@ val = login()
     # see and do faculty related activities
 # elif val[1] = 'advisor':
     # see and do advisor related activities
+
+
+if val[1] == 'student':
+    user = Student()
+    ans1 = user.first_menu()
+    print(ans1)
+    if ans1 == 1:
+        user.see_request()
+    elif ans1 == 2:
+        if user.num_request != 0:
+            print('You must deny all member request first.')
+            user.first_menu()
+        else:
+            user.to_lead()
+
+
+
+
+
+
 
 # once everyhthing is done, make a call to the exit function
 exit()
